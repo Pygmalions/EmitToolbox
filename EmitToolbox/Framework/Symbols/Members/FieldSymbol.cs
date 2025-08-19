f0ffb79b017d58b0ee38e979e2acadb9897a2aef
Haoyu Jia <jia_vincent@outlook.com>
2025-08-19 21:28:00 +0900

    Separate static field and static property into independent classes.

---
+++ EmitToolbox/Framework/Symbols/Members/FieldSymbol.cs
@@ -0,0 +1,97 @@
+using System.Linq.Expressions;
+
+namespace EmitToolbox.Framework.Symbols.Members;
+
+public class FieldSymbol<TValue> : VariableSymbol<TValue>
+{
+    public FieldSymbol(MethodBuildingContext context, ValueSymbol target, FieldInfo field)
+        : base(context, field.FieldType.IsByRef)
+    {
+        if (field.IsStatic)
+            throw new ArgumentException("Cannot create an instance field symbol for a static field.", nameof(field));
+        if (!target.ValueType.IsAssignableTo(field.DeclaringType))
+            throw new ArgumentException(
+                "Target type is not assignable to the declaring type of the field.", nameof(target));
+        Target = target;
+        Field = field;
+    }
+
+    /// <summary>
+    /// Target symbol for the field.
+    /// </summary>
+    public ValueSymbol Target { get; }
+
+    /// <summary>
+    /// Field wrapped in this symbol.
+    /// </summary>
+    public FieldInfo Field { get; }
+
+    protected override void EmitDirectlyStoreValue()
+    {
+        TemporaryVariable.EmitStoreFromValue();
+        Target.EmitLoadAsTarget();
+        TemporaryVariable.EmitLoadAsValue();
+        Context.Code.Emit(OpCodes.Stfld, Field);
+    }
+
+    protected internal override void EmitDirectlyLoadValue()
+    {
+        Target.EmitLoadAsTarget();
+        Context.Code.Emit(OpCodes.Ldfld, Field);
+    }
+
+    protected internal override void EmitDirectlyLoadAddress()
+    {
+        Target.EmitLoadAsTarget();
+        Context.Code.Emit(OpCodes.Ldflda, Field);
+    }
+}
+
+public class StaticFieldSymbol<TValue> : VariableSymbol<TValue>
+{
+    public StaticFieldSymbol(MethodBuildingContext context, FieldInfo field)
+        : base(context, field.FieldType.IsByRef)
+    {
+        if (!field.IsStatic)
+            throw new ArgumentException("Cannot create a static field symbol for an instance field.", nameof(field));
+        Field = field;
+    }
+
+    public FieldInfo Field { get; }
+
+    protected override void EmitDirectlyStoreValue()
+    {
+        Context.Code.Emit(OpCodes.Stsfld, Field);
+    }
+
+    protected internal override void EmitDirectlyLoadValue()
+    {
+        Context.Code.Emit(OpCodes.Ldsfld, Field);
+    }
+
+    protected internal override void EmitDirectlyLoadAddress()
+    {
+        Context.Code.Emit(OpCodes.Ldsflda, Field);
+    }
+}
+
+public static class FieldSymbolExtension
+{
+    public static FieldSymbol<TField> GetField<TTarget, TField>(
+        this ValueSymbol<TTarget> target, Expression<Func<TTarget, TField>> expression)
+    {
+        return expression.Body is not MemberExpression memberExpression
+            ? throw new ArgumentException("Expression must be a field access expression.", nameof(expression))
+            : new FieldSymbol<TField>(target.Context, target, (FieldInfo)memberExpression.Member);
+    }
+
+    public static FieldSymbol<TField> SetField<TTarget, TField, TValue>(
+        this ValueSymbol<TTarget> target, Expression<Func<TTarget, TField>> expression,
+        ValueSymbol<TValue> value)
+        where TValue : TField
+    {
+        var field = GetField(target, expression);
+        field.Assign(value);
+        return field;
+    }
+}
---
+++ EmitToolbox/Framework/Symbols/Members/PropertySymbol.cs
@@ -0,0 +1,122 @@
+using System.Linq.Expressions;
+
+namespace EmitToolbox.Framework.Symbols.Members;
+
+public class PropertySymbol<TValue> : VariableSymbol<TValue>
+{
+    public ValueSymbol Target { get; }
+
+    public PropertyInfo Property { get; }
+
+    public bool EnableVirtualCalling { get; init; } = true;
+
+    public bool HasGetter { get; }
+
+    public bool HasSetter { get; }
+
+    public PropertySymbol(MethodBuildingContext context, ValueSymbol target, PropertyInfo property)
+        : base(context, property.PropertyType.IsByRef)
+    {
+        if (property.GetMethod?.IsStatic == true || property.SetMethod?.IsStatic == true)
+            throw new ArgumentException("Cannot create an instance property symbol for a static property.",
+                nameof(property));
+        if (!target.ValueType.IsAssignableTo(property.DeclaringType))
+            throw new ArgumentException(
+                "Target type is not assignable to the declaring type of the property.", nameof(target));
+        Target = target;
+        Property = property;
+        HasGetter = property.GetMethod != null;
+        HasSetter = property.SetMethod != null;
+    }
+
+    protected override void EmitDirectlyStoreValue()
+    {
+        if (Property.SetMethod == null)
+            throw new InvalidOperationException($"Property '{Property.Name}' does not have a setter.");
+
+        TemporaryVariable.EmitStoreFromValue();
+        Target.EmitLoadAsTarget();
+        TemporaryVariable.EmitLoadAsValue();
+        Context.Code.Emit(EnableVirtualCalling && Property.SetMethod.IsVirtual ? OpCodes.Callvirt : OpCodes.Call,
+            Property.SetMethod);
+    }
+
+    protected internal override void EmitDirectlyLoadValue()
+    {
+        if (Property.GetMethod == null)
+            throw new InvalidOperationException($"Property '{Property.Name}' does not have a getter.");
+        Target.EmitLoadAsTarget();
+        Context.Code.Emit(EnableVirtualCalling && Property.GetMethod.IsVirtual ? OpCodes.Callvirt : OpCodes.Call,
+            Property.GetMethod);
+    }
+
+    protected internal override void EmitDirectlyLoadAddress()
+    {
+        var value = Context.Variable<TValue>();
+        value.Assign(this);
+        value.EmitLoadAsAddress();
+    }
+}
+
+public class StaticPropertySymbol<TValue> : VariableSymbol<TValue>
+{
+    public PropertyInfo Property { get; }
+
+    public bool HasGetter { get; }
+
+    public bool HasSetter { get; }
+
+    public StaticPropertySymbol(MethodBuildingContext context, PropertyInfo property) : base(context)
+    {
+        if (property.GetMethod?.IsStatic == false || property.SetMethod?.IsStatic == false)
+            throw new ArgumentException("Cannot create a static property symbol for an instance property.",
+                nameof(property));
+        Property = property;
+        HasGetter = property.GetMethod != null;
+        HasSetter = property.SetMethod != null;
+    }
+
+    protected override void EmitDirectlyStoreValue()
+    {
+        if (Property.SetMethod == null)
+            throw new InvalidOperationException($"Property '{Property.Name}' does not have a setter.");
+
+        Context.Code.Emit(OpCodes.Call, Property.SetMethod);
+    }
+
+    protected internal override void EmitDirectlyLoadValue()
+    {
+        if (Property.GetMethod == null)
+            throw new InvalidOperationException($"Property '{Property.Name}' does not have a getter.");
+
+        Context.Code.Emit(OpCodes.Call, Property.GetMethod);
+    }
+
+    protected internal override void EmitDirectlyLoadAddress()
+    {
+        var value = Context.Variable<TValue>();
+        value.Assign(this);
+        value.EmitLoadAsAddress();
+    }
+}
+
+public static class PropertyElementExtension
+{
+    public static PropertySymbol<TValue> GetProperty<TTarget, TValue>(
+        this ValueSymbol<TTarget> target, Expression<Func<TTarget, TValue>> expression)
+    {
+        return expression.Body is not MemberExpression memberExpression
+            ? throw new ArgumentException("Expression must be a property access expression.", nameof(expression))
+            : new PropertySymbol<TValue>(target.Context, target, (PropertyInfo)memberExpression.Member);
+    }
+
+    public static PropertySymbol<TProperty> SetProperty<TTarget, TProperty, TValue>(
+        this ValueSymbol<TTarget> target, Expression<Func<TTarget, TProperty>> expression,
+        ValueSymbol<TValue> value)
+        where TValue : TProperty
+    {
+        var property = GetProperty(target, expression);
+        property.Assign(value);
+        return property;
+    }
+}
