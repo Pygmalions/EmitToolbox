d66d745cbb9cc45e416f60a71c3b6febe615fa14
Haoyu Jia <jia_vincent@outlook.com>
2025-08-21 21:02:48 +0900

    Add symbol to access `this` argument.

---
+++ EmitToolbox/Framework/Symbols/ThisSymbol.cs
@@ -0,0 +1,71 @@
+using System.Diagnostics.CodeAnalysis;
+using EmitToolbox.Framework.Symbols.Utilities;
+
+namespace EmitToolbox.Framework.Symbols;
+
+public class ThisSymbol(MethodBuildingContext context, Type type) : ValueSymbol(context, type.IsValueType)
+{
+    public override Type ValueType { get; } = type;
+
+    [field: MaybeNull]
+    private Action<ILGenerator> ReferenceLoader =>
+        field ??= ValueIndirectlyLoader.GetReferenceLoader(ValueType);
+    
+    protected internal override void EmitDirectlyLoadValue()
+    {
+        Context.Code.Emit(OpCodes.Ldarg_0);
+    }
+
+    protected internal override void EmitDirectlyLoadAddress()
+    {
+        Context.Code.Emit(OpCodes.Ldarga_S, 0);
+    }
+
+    protected internal override void EmitLoadAsValue()
+    {
+        EmitDirectlyLoadValue();
+        if (IsReference)
+            ReferenceLoader(Context.Code);
+    }
+
+    protected internal override void EmitLoadAsAddress()
+    {
+        if (!IsReference)
+            EmitDirectlyLoadAddress();
+        else 
+            EmitDirectlyLoadValue();
+    }
+}
+
+public class ThisSymbol<TType>(MethodBuildingContext context) 
+    : ValueSymbol<TType>(context, typeof(TType).IsValueType)
+{
+    [field: MaybeNull]
+    private Action<ILGenerator> ReferenceLoader =>
+        field ??= ValueIndirectlyLoader.GetReferenceLoader(typeof(TType));
+    
+    protected internal override void EmitDirectlyLoadValue()
+    {
+        Context.Code.Emit(OpCodes.Ldarg_0);
+    }
+
+    protected internal override void EmitDirectlyLoadAddress()
+    {
+        Context.Code.Emit(OpCodes.Ldarga_S, 0);
+    }
+
+    protected internal override void EmitLoadAsValue()
+    {
+        EmitDirectlyLoadValue();
+        if (IsReference)
+            ReferenceLoader(Context.Code);
+    }
+
+    protected internal override void EmitLoadAsAddress()
+    {
+        if (!IsReference)
+            EmitDirectlyLoadAddress();
+        else 
+            EmitDirectlyLoadValue();
+    }
+}
