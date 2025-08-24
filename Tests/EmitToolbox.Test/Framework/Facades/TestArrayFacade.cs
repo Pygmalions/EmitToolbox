aece9d0026168906e05f982ef4d750788fcd4e94
Haoyu Jia <jia_vincent@outlook.com>
2025-08-24 17:02:08 +0900

    Add unit test for array facade.

---
+++ Tests/EmitToolbox.Test/Framework/Facades/TestArrayFacade.cs
@@ -0,0 +1,79 @@
+using EmitToolbox.Framework;
+using EmitToolbox.Framework.Symbols.Facades;
+
+namespace EmitToolbox.Test.Framework.Facades;
+
+[TestFixture]
+public class TestArrayFacade
+{
+    private static AssemblyBuildingContext _assembly;
+
+    [SetUp]
+    public void Initialize()
+    {
+        _assembly = AssemblyBuildingContext
+            .CreateExecutableContextBuilder("TestLiteralSymbol")
+            .Build();
+    }
+
+    [Test]
+    public void TestArrayFacade_StoreElement()
+    {
+        var typeContext = _assembly.DefineClass("TestArrayFacade_Store");
+        var methodContext = typeContext.DefineStaticAction("Test", [ParameterDefinition.Value<int[]>()]);
+        var array = methodContext.Argument<int[]>(0).AsArray();
+
+        var value = TestContext.CurrentContext.Random.Next();
+
+        array.Assign(0, methodContext.Value(value));
+
+        methodContext.Return();
+
+        typeContext.Build();
+
+        int[] argument = [1, 2, 3, 4, 5];
+
+        methodContext.BuildingMethod.Invoke(null, [argument]);
+        Assert.That(argument[0], Is.EqualTo(value));
+    }
+
+    [Test]
+    public void TestArrayFacade_LoadElement()
+    {
+        var typeContext = _assembly.DefineClass("TestArrayFacade_Store");
+        var methodContext = typeContext.DefineStaticFunctor("Test",
+            [ParameterDefinition.Value<int[]>()],
+            ResultDefinition.Value<int>());
+        var array = methodContext.Argument<int[]>(0).AsArray();
+        methodContext.Return(array[0]);
+        typeContext.Build();
+
+        var value = TestContext.CurrentContext.Random.Next();
+        int[] argument = [value, 2, 3, 4, 5];
+
+        Assert.That(methodContext.BuildingMethod.Invoke(null, [argument]),
+            Is.EqualTo(value));
+    }
+
+    [Test]
+    public void TestArrayFacade_LoadElement_DynamicIndex()
+    {
+        var typeContext = _assembly.DefineClass("TestArrayFacade_Store");
+        var methodContext = typeContext.DefineStaticFunctor("Test",
+            [
+                ParameterDefinition.Value<int[]>("array"),
+                ParameterDefinition.Value<int>("index")
+            ],
+            ResultDefinition.Value<int>());
+        var parameterArray = methodContext.Argument<int[]>(0).AsArray();
+        var parameterIndex = methodContext.Argument<int>(1);
+        methodContext.Return(parameterArray[parameterIndex]);
+        typeContext.Build();
+        
+        int[] array = [1, 2, 3, 4, 5];
+        var index = TestContext.CurrentContext.Random.Next(0, array.Length);
+
+        Assert.That(methodContext.BuildingMethod.Invoke(null, [array, index]),
+            Is.EqualTo(array[index]));
+    }
+}
