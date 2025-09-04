fe56317e176b93704558d8a80f1a490010dde495
Haoyu Jia <jia_vincent@outlook.com>
2025-09-04 19:43:48 +0900

    'StringSymbol' now uses static 'Equals' method to compare values.

--- EmitToolbox/Framework/Symbols/Extensions/ValueSymbol.String.cs
+++ EmitToolbox/Framework/Symbols/Extensions/ValueSymbol.String.cs
@@ -6,26 +6,32 @@ public static class ValueSymbolStringExtensions
     {
         if (target.Context != other.Context)
             throw new InvalidOperationException("Cannot compare values from different contexts.");
-        
+
         var result = target.Context.Variable<bool>();
-        
+
         target.EmitLoadAsValue();
         other.EmitLoadAsValue();
-        target.Context.Code.Emit(OpCodes.Call, typeof(string).GetMethod("op_Equality")!);
+        target.Context.Code.Emit(OpCodes.Call,
+            typeof(string).GetMethod("Equals",
+                BindingFlags.Static | BindingFlags.Public,
+                [typeof(string), typeof(string)])!);
         result.EmitStoreFromValue();
-        
+
         return result;
     }
-    
+
     public static VariableSymbol<bool> IsEqualTo(this ValueSymbol<string> target, string other)
     {
         var result = target.Context.Variable<bool>();
-        
+
         target.EmitLoadAsValue();
         target.Context.Code.Emit(OpCodes.Ldstr, other);
-        target.Context.Code.Emit(OpCodes.Call, typeof(string).GetMethod("op_Equality")!);
+        target.Context.Code.Emit(OpCodes.Call,
+            typeof(string).GetMethod("Equals",
+                BindingFlags.Static | BindingFlags.Public,
+                [typeof(string), typeof(string)])!);
         result.EmitStoreFromValue();
-        
+
         return result;
     }
 }
