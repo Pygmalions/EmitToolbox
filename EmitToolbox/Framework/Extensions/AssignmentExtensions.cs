93b9d247204ca1b4ed62e216862529ffcd1844fd
Haoyu Jia <jia_vincent@outlook.com>
2025-11-11 20:24:17 +0900

    Fix the bugs when the destination holds a reference to a reference type.

--- EmitToolbox/Framework/Extensions/AssignmentExtensions.cs
+++ EmitToolbox/Framework/Extensions/AssignmentExtensions.cs
@@ -5,6 +5,51 @@ namespace EmitToolbox.Framework.Extensions;
 
 public static class AssignmentExtensions
 {
+    private static void CopyValue(ISymbol source, ISymbol destination)
+    {
+        var basicType = destination.BasicType;
+        var code =
+            CrossContextException.EnsureContext(source, destination).Code;
+
+        // When the destination holds a value type:
+        if (basicType.IsValueType)
+        {
+            // When the destination holds a reference or is addressable:
+            if (destination.CanLoadAsReference)
+            {
+                destination.LoadAsReference();
+
+                if (source.CanLoadAsReference)
+                {
+                    source.LoadAsReference();
+                    code.Emit(OpCodes.Cpobj, basicType);
+                    return;
+                }
+
+                source.LoadAsValue();
+                if (basicType.IsPrimitive)
+                    code.Emit(PrimitiveTypeMetadata.GetInstructionForIndirectStoring(basicType));
+                else
+                    code.Emit(OpCodes.Stobj, basicType);
+                return;
+            }
+        }
+        // When the destination holds a reference to a reference type:
+        else if (destination.ContentType.IsByRef)
+        {
+            destination.LoadContent();
+            source.LoadAsValue();
+            code.Emit(OpCodes.Stind_Ref);
+            return;
+        }
+
+        if (destination is not IAssignableSymbol assignable)
+            throw new InvalidOperationException(
+                "Cannot copy value: the destination symbol is not a reference nor assignable.");
+
+        assignable.AssignContent(source);
+    }
+
     /// <summary>
     /// Assign the value of the source symbol to the destination symbol (this symbol).
     /// If the source and destination are both value types and can be loaded as references,
@@ -16,8 +61,8 @@ public static class AssignmentExtensions
     public static void AssignValue<TContent>(
         this IAssignableSymbol<TContent> destination, ISymbol<TContent> source)
         where TContent : allows ref struct
-        => destination.CopyValueFrom(source);
-    
+        => CopyValue(source, destination);
+
     /// <summary>
     /// Assign the value of the source symbol to the destination symbol (this symbol).
     /// If the source and destination are both value types and can be loaded as references,
@@ -26,8 +71,8 @@ public static class AssignmentExtensions
     /// <param name="destination">Destination symbol to copy value to.</param>
     /// <param name="source">Source symbol to copy value from.</param>
     public static void AssignValue(this IAssignableSymbol destination, ISymbol source)
-        => destination.CopyValueFrom(source);
-    
+        => CopyValue(source, destination);
+
     extension<TContent>(ISymbol<TContent> self) where TContent : allows ref struct
     {
         /// <summary>
@@ -40,40 +85,8 @@ public static class AssignmentExtensions
         /// Thrown when the destination symbol is not a reference nor assignable.
         /// </exception>
         public void CopyValueFrom(ISymbol<TContent> source)
-        {
-            var code = self.Context.Code;
-            
-            var type = typeof(TContent);
-            
-            if (type.IsValueType)
-            {
-                if (self.CanLoadAsReference)
-                {
-                    self.LoadAsReference();
-            
-                    if (source.CanLoadAsReference)
-                    {
-                        source.LoadAsReference();
-                        code.Emit(OpCodes.Cpobj, type);
-                        return;
-                    }
-            
-                    source.LoadAsValue();
-                    if (type.IsPrimitive)
-                        code.Emit(PrimitiveTypeMetadata<TContent>.InstructionForIndirectStoring.Value);
-                    else
-                        code.Emit(OpCodes.Stobj, type);
-                    return;
-                }
-            }
-        
-            if (self is not IAssignableSymbol<TContent> assignable)
-                throw new InvalidOperationException(
-                    "Cannot copy value from this symbol to this symbol: it is not a reference nor assignable.");
-        
-            assignable.AssignContent(source);
-        }
-        
+            => CopyValue(source, self);
+
         /// <summary>
         /// Copy the value of the source symbol (this symbol) to the destination symbol.
         /// If the source and destination are both value types and can be loaded as references,
@@ -84,7 +97,7 @@ public static class AssignmentExtensions
         /// Thrown when the destination symbol is not a reference nor assignable.
         /// </exception>
         public void CopyValueTo(ISymbol<TContent> destination)
-            => destination.CopyValueFrom(self);
+            => CopyValue(self, destination);
     }
 
     extension(ISymbol self)
@@ -99,45 +112,8 @@ public static class AssignmentExtensions
         /// Thrown when the destination symbol is not a reference nor assignable.
         /// </exception>
         public void CopyValueFrom(ISymbol source)
-        {
-            var code = self.Context.Code;
-            
-            var type = source.BasicType;
+            => CopyValue(source, self);
 
-            if (type.IsValueType && type != self.BasicType ||
-                !self.BasicType.IsAssignableFrom(type))
-                throw new InvalidOperationException(
-                    $"Cannot assign value of type '{type}' to symbol of type '{self.BasicType}'.");
-            
-            if (type.IsValueType)
-            {
-                if (self.CanLoadAsReference)
-                {
-                    self.LoadAsReference();
-            
-                    if (source.CanLoadAsReference)
-                    {
-                        source.LoadAsReference();
-                        code.Emit(OpCodes.Cpobj, type);
-                        return;
-                    }
-            
-                    source.LoadAsValue();
-                    if (type.IsPrimitive)
-                        code.Emit(PrimitiveTypeMetadata.GetInstructionForIndirectStoring(type));
-                    else
-                        code.Emit(OpCodes.Stobj, type);
-                    return;
-                }
-            }
-        
-            if (self is not IAssignableSymbol assignable)
-                throw new InvalidOperationException(
-                    "Cannot copy value from this symbol to this symbol: it is not a reference nor assignable.");
-        
-            assignable.AssignContent(source);
-        }
-        
         /// <summary>
         /// Copy the value of the source symbol (this symbol) to the destination symbol.
         /// If the source and destination are both value types and can be loaded as references,
@@ -148,6 +124,6 @@ public static class AssignmentExtensions
         /// Thrown when the destination symbol is not a reference nor assignable.
         /// </exception>
         public void CopyValueTo(ISymbol destination)
-            => destination.CopyValueFrom(self);
+            => CopyValue(self, destination);
     }
 }
