36fd8bd867c6a5ecd4b5eeac315e8013660e43b4
Haoyu Jia <jia_vincent@outlook.com>
2025-11-17 21:03:51 +0900

    Add extension methods for constructing and throwing exceptions.

---
+++ Tests/EmitToolbox.Test/Framework/Extensions/TestExceptionExtensions.cs
@@ -0,0 +1,84 @@
+using EmitToolbox.Framework;
+using EmitToolbox.Framework.Extensions;
+using EmitToolbox.Framework.Symbols;
+using EmitToolbox.Framework.Utilities;
+
+namespace EmitToolbox.Test.Framework.Extensions;
+
+[TestFixture, TestOf(typeof(ExceptionExtensions))]
+public class TestExceptionExtensions
+{
+    private DynamicAssembly _assembly;
+
+    [SetUp]
+    public void Setup()
+    {
+        _assembly = DynamicAssembly.DefineExecutable(Guid.CreateVersion7().ToString());
+    }
+    
+    [Test]
+    public void Throw_WithoutMessage()
+    {
+        var type = _assembly.DefineClass(Guid.CreateVersion7().ToString());
+        var method = type.MethodFactory.Static.DefineAction(nameof(Throw_WithoutMessage));
+        method.ThrowException();
+        method.Return();
+        
+        type.Build();
+        var functor = method.BuildingMethod.CreateDelegate<Action>();
+        Assert.Throws<Exception>(() => functor());
+    }
+    
+    [Test]
+    public void Throw_WithoutMessage_Generic()
+    {
+        var type = _assembly.DefineClass(Guid.CreateVersion7().ToString());
+        var method = type.MethodFactory.Static.DefineAction(nameof(Throw_WithoutMessage_Generic));
+        method.ThrowException<ArgumentException>();
+        method.Return();
+        
+        type.Build();
+        var functor = method.BuildingMethod.CreateDelegate<Action>();
+        Assert.Throws<ArgumentException>(() => functor());
+    }
+    
+    [Test]
+    public void Throw_WithMessage()
+    {
+        var type = _assembly.DefineClass(Guid.CreateVersion7().ToString());
+        var method = type.MethodFactory.Static.DefineAction(nameof(Throw_WithoutMessage));
+        method.ThrowException("Test");
+        method.Return();
+        
+        type.Build();
+        var functor = method.BuildingMethod.CreateDelegate<Action>();
+        Assert.Throws<Exception>(() => functor(), "Test");
+    }
+    
+    [Test]
+    public void Throw_WithMessage_Generic()
+    {
+        var type = _assembly.DefineClass(Guid.CreateVersion7().ToString());
+        var method = type.MethodFactory.Static.DefineAction(nameof(Throw_WithoutMessage_Generic));
+        method.ThrowException<ArgumentException>("Test");
+        method.Return();
+        
+        type.Build();
+        var functor = method.BuildingMethod.CreateDelegate<Action>();
+        Assert.Throws<ArgumentException>(() => functor(), "Test");
+    }
+    
+    [Test]
+    public void Throw_WithMessage_Selector()
+    {
+        var type = _assembly.DefineClass(Guid.CreateVersion7().ToString());
+        var method = type.MethodFactory.Static.DefineAction(nameof(Throw_WithoutMessage_Generic));
+        method.ThrowException(() => new ArgumentException(Any<string>.Value),
+            [method.Value("Test")]);
+        method.Return();
+        
+        type.Build();
+        var functor = method.BuildingMethod.CreateDelegate<Action>();
+        Assert.Throws<ArgumentException>(() => functor(), "Test");
+    }
+}
