54fe2a83a4a25ce73de957c576a9a32cc7d87f35
Haoyu Jia <jia_vincent@outlook.com>
2025-12-24 20:44:01 +0900

    Fix the wrong understanding of 'DefineDefaultConstructor'; now it correctly define and implement a default constructor that only invokes the base constructor.

--- EmitToolbox/Builders/ConstructorBuilderFacade.cs
+++ EmitToolbox/Builders/ConstructorBuilderFacade.cs
@@ -6,7 +6,22 @@ public class ConstructorBuilderFacade(DynamicType context)
 {
     [MustUseReturnValue]
     public DynamicConstructor Define(VisibilityLevel visibility = VisibilityLevel.Public)
+    /// <summary>
+    /// Define a parameterless constructor that simply calls the parent constructor.
+    /// Note that its <see cref="ConstructorBuilder.GetILGenerator()"/> method is not allowed to access.
+    /// </summary>
+    /// <param name="visibility">
+    /// Visibility of this constructor.
+    /// Note that a struct must have a public parameterless constructor.
+    /// </param>
+    /// <param name="configure">Functor that further configures the constructor builder.</param>
+    public void DefineDefaultConstructor(VisibilityLevel visibility = VisibilityLevel.Public,
+        Action<ConstructorBuilder>? configure = null)
     {
+        if (context.Builder.BaseType == typeof(ValueType) && visibility != VisibilityLevel.Public)
+            throw new ArgumentException(
+                "Failed to define the default constructor: " +
+                "the parameterless constructor of a struct cannot be non-public.");
         var attributes = MethodAttributes.HideBySig | MethodAttributes.SpecialName |
                          MethodAttributes.RTSpecialName | visibility.ToMethodAttributes();
         var builder = context.Builder.DefineDefaultConstructor(attributes);
@@ -18,6 +33,7 @@ public class ConstructorBuilderFacade(DynamicType context)
             ParameterTypes = Type.EmptyTypes,
             ReturnType = typeof(void),
         };
+        configure?.Invoke(builder);
     }
 
     [MustUseReturnValue]
